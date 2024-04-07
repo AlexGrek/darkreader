da2386153e2421e8cc5a058e96b27523972fe91a
alexgrek <alexgrek.hq@gmail.com>
2024-04-07 04:41:09 +0300

    playing with fonts

---
+++ frontend/src/components/AboutMe.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+interface AppProps {
+}
+
+const App: React.FC<AppProps> = ({ }) => {
+    return (
+        <div>
+            <p>
+                I want some dark cyberpunk-noir-like stuff. My favorite movie is Blade Runner 2049. I also like Ghost in the shell (including anime), Nolan's batman series and other, Dune, Drive, Equilibrium, Oblivion, Alita, Ex Machina, Psychopass anime series, Upgrade. I like games like Cyberpunk, Prey 2017, Dishonored, Mass Effect series, System Shock. I listen to dark ambient, synthwave and retrowave cinematic music alongside with post-hardcore.
+            </p>
+            <p>
+                Cultural background:
+                I have grown in a post-soviet city.
+                I was always impressed by Soviet brutalism architecture.
+            </p>
+        </div>
+    );
+};
+
+export default App;
